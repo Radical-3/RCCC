256905bc7bfe591523ff38ea49fa56463cecb67e
LiuHaoji <lhj@qq.com>
2024-05-13 15:40:51 +0800

    修改原始代码里的引用，以便在包中调用

---
+++ test_script.py
@@ -0,0 +1,26 @@
+import cv2
+import torch
+from torch import optim
+
+from detector.detector_controller import detector_controller
+from config import Config
+from log import logger
+
+config = Config(logger, './config/base.yaml').item()
+logger.set_config(config)
+detector = detector_controller(config, "yolov6")
+image = cv2.imread("data/dataset/image4.png")
+image_tensor = torch.load("data/dataset/1.pt")
+image_tensor.require_grad = True
+optimizer = optim.Adam([image_tensor], lr=0.01, amsgrad=True)
+for i in range(3):
+    # result = detector.detect(image)
+    result = detector.run(image_tensor)
+    class_confs = result[:, :, 5:5 + 80]
+    class_confs = torch.nn.Softmax(dim=2)(class_confs)
+    class_confs = class_confs[:, :, 2]
+    objectiveness_score = result[:, :, 4]
+    confs_if_object = objectiveness_score * class_confs
+    max_conf, _ = torch.max(confs_if_object, dim=1)
+    loss = max_conf
+    loss.backward()
