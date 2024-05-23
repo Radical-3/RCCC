1d6b1e9dc681e0a70d2bf6a4c4922b81c4647018
ZhaoZhiqiao <zhao_zhiqiao@outlook.com>
2024-05-23 17:53:08 +0800

    [feature] 为训练过程中的样本添加随机噪声

--- loss/utils.py
+++ loss/utils.py
@@ -6,6 +6,8 @@ def transform(config, image):
         image = random_brightness(config, image)
     if config.add_contrast:
         image = random_contrast(config, image)
+    if config.add_noise:
+        image = random_noise(config, image)
     return image
 
 
@@ -19,5 +21,12 @@ def random_brightness(config, image):
 def random_contrast(config, image):
     contrast = torch.rand(1, device=config.device).uniform_(config.min_contrast, config.max_contrast)
     image = image * contrast
+    image.clamp_(0, 1)
+    return image
+
+
+def random_noise(config, image):
+    noise = torch.randn(image.shape, device=config.device) * config.noise_intensity
+    image = image + noise
     image.clamp_(0, 1)
     return image
